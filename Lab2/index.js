342858595a37da2844cad8707307dd4d0d2ebd1a
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-09-06 16:52:10 +0300

    Split index.html on index.js and main.css

---
+++ Lab2/index.js
@@ -0,0 +1,89 @@
+const addTrToTable = (table) => {
+  const row = document.createElement('tr');
+  table.appendChild(row);
+};
+
+const addTdToTable = (table, row, colspan, rowspan, className) => {
+  const td = document.createElement('td');
+  if (colspan) td.setAttribute('colspan', colspan);
+  if (rowspan) td.setAttribute('rowspan', rowspan);
+  if (className) td.className = className;
+  table.children[row].appendChild(td);
+};
+
+const createTable = (captionText) => {
+  const table = document.createElement('table');
+  const caption = document.createElement('caption');
+  caption.innerText = captionText;
+  table.appendChild(caption);
+  return table;
+};
+
+const tablesCreators = [
+  (table) => {
+    let curRow = 1;
+    new Array(7).fill(null).forEach((elem, i) => {
+      addTrToTable(table);
+      addTdToTable(table, curRow, 7-i, 0);
+      if (i !== 0) {
+        addTdToTable(table, curRow++, 0, 7-i);
+      } else curRow++;
+    });
+    return table;
+  },
+  
+  (table) => {
+    let curRow = 1;
+    new Array(7).fill(null).forEach((elem, i) => {
+      addTrToTable(table);
+      addTdToTable(table, curRow, 6-i, 0);
+      if (i !== 6)addTdToTable(table, curRow++, 0, 7-i);
+    });
+    return table;
+  },
+  
+  (table) => {
+    addTrToTable(table);
+    
+    new Array(6).fill(null).forEach((elem, j) => {
+      let count = j % 2 ? 3 : 4;
+      addTrToTable(table);
+      for (let i = 0 ; i < count ; i++) addTdToTable(table, j+2, 0, 2);
+    });
+    
+    for (let i = 0 ; i < 7 ; i++) {
+      if (i % 2) {
+        addTdToTable(table, 1, 0, 2);
+      } else {
+        addTdToTable(table, 1);
+      }
+    }
+    new Array(7).fill(null).forEach((elem, i) => {
+      addTdToTable(table, i+1, 0, 0, 'hidden-td')
+    });
+    
+    return table;
+  },
+  
+  (table) => {
+    const diff = [[3, 1], [1, 3], [2, 2]];
+    
+    new Array(7).fill(null).forEach((elem, i) => {
+      let k = i%3;
+    addTrToTable(table);
+    addTdToTable(table, i+1, diff[k][0]);
+    addTdToTable(table, i+1, 3);
+    addTdToTable(table, i+1, diff[k][1]);
+    });
+    
+    addTrToTable(table);
+    new Array(7).fill(null).forEach(() => { addTdToTable(table, 8, 0, 0, 'hidden-td') });
+    
+    return table;
+  }
+];
+
+for (let i = 0 ; i < 4 ; i++) {
+  const table = tablesCreators[i](createTable('Table' + (i+1)));
+  document.body.appendChild(table);
+}
