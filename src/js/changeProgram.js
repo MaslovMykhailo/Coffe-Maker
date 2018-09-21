a8cb0e0d9635abb3785ba0a16cc594475cd8be88
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-09-21 20:57:26 +0300

    Add change of a program on click to link

---
+++ src/js/changeProgram.js
@@ -0,0 +1,27 @@
+var linkList = document.getElementById('programs-menu').children[0].children;
+var programList = [
+    document.getElementById('basic'),
+    document.getElementById('premium'),
+    document.getElementById('platinum')
+];
+
+
+function changeProgram() {
+    for (var i = 0 ; i < 3 ; i++) {
+        linkList[i].classList.remove('active');
+        programList[i].classList.remove('visible');
+        programList[i].classList.add('hidden');
+    }
+
+    var chooseProgram = document.getElementById(this.innerText.toLowerCase());
+    chooseProgram.classList.add('visible');
+    chooseProgram.style.opacity = '0';
+    this.parentElement.classList.add('active');
+    setTimeout(function () {
+        chooseProgram.style.opacity = '1';
+    }, 15);
+}
+
+for (var j = 0 ; j < 3 ; j++) {
+    linkList[j].children[0].onclick = changeProgram;
+}
